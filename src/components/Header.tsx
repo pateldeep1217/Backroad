866a61c60119905153a8dbca16874a68473fff2c
Deep Patel <patel_deep1217@outlook.com>
2024-05-25 14:21:13 -0400

    feat: Add Logo component and switch icon library

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Logo from "./Logo";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="">
+      <div className="container mx-auto  flex h-16 items-center">
+        <Logo />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
