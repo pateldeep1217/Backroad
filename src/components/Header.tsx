e596e12ab746b8a4c91611254c7ff6c61526b41d
Deep Patel <patel_deep1217@outlook.com>
2024-05-25 15:09:15 -0400

    style: implement hover effects for hamburger icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
     <header className="">
       <div className="container mx-auto  flex h-16 items-center justify-between">
         <Logo />
-        <IconMenu2 className="text-primary-5 cursor-pointer font-bold" />
+        <IconMenu2 className="text-primary-5 cursor-pointer font-bold hover:size-8 hover:transition-all hover:duration-300" />
       </div>
     </header>
   );
