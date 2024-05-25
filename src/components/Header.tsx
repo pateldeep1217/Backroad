aeaf3f43f20e7b2d8198bbed552257236f599d4c
Deep Patel <patel_deep1217@outlook.com>
2024-05-25 14:56:56 -0400

    feat: Add hamburger Menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Logo from "./Logo";
+import { IconMenu2 } from "@tabler/icons-react";
 
 function Header() {
   return (
     <header className="">
-      <div className="container mx-auto  flex h-16 items-center">
+      <div className="container mx-auto  flex h-16 items-center justify-between">
         <Logo />
+        <IconMenu2 className="text-primary-5 cursor-pointer font-bold" />
       </div>
     </header>
   );
