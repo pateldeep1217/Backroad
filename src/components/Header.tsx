07c5686a4d4e093e9da80f2962e26b2ca4c92e0d
Deep Patel <patel_deep1217@outlook.com>
2024-05-27 16:08:22 -0400

    feat: Make navbar fixed to remain on top during scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,26 +16,28 @@ function Header() {
     setShowLinks((prevState) => !prevState);
   }
   return (
-    <header className=" mx-auto  md:container md:flex md:items-center md:justify-between">
-      <div className="flex h-16 items-center justify-between  px-8 md:p-0">
-        <Logo />
-        <IconMenu2
-          className="cursor-pointer font-bold text-primary-5  hover:size-9 hover:transition-all hover:duration-300 md:hidden"
-          size={30}
-          onClick={toggleMenu}
-        />
-      </div>
+    <header className=" fixed left-0 right-0 top-0  bg-white">
+      <div className="container mx-auto md:flex md:items-center md:justify-between">
+        <div className="flex h-16 items-center justify-between  px-8 md:p-0">
+          <Logo />
+          <IconMenu2
+            className="cursor-pointer font-bold text-primary-5  hover:size-9 hover:transition-all hover:duration-300 md:hidden"
+            size={30}
+            onClick={toggleMenu}
+          />
+        </div>
 
-      <nav
-        className={` transition-max-height  overflow-hidden duration-700 md:flex md:items-center md:gap-5 md:overflow-visible ${showLinks ? "max-h-64 " : "max-h-0"}`}
-      >
-        <NavLinks />
-      </nav>
+        <nav
+          className={` transition-max-height  overflow-hidden duration-700 md:flex md:items-center md:gap-5 md:overflow-visible ${showLinks ? "max-h-64 " : "max-h-0"}`}
+        >
+          <NavLinks />
+        </nav>
 
-      <div className=" hidden  gap-2  md:flex md:items-center">
-        <SocialMediaIcon icon={<IconBrandFacebook />} />
-        <SocialMediaIcon icon={<IconBrandTwitter />} />
-        <SocialMediaIcon icon={<IconBrandInstagram />} />
+        <div className=" hidden  gap-2  md:flex md:items-center">
+          <SocialMediaIcon icon={<IconBrandFacebook />} />
+          <SocialMediaIcon icon={<IconBrandTwitter />} />
+          <SocialMediaIcon icon={<IconBrandInstagram />} />
+        </div>
       </div>
     </header>
   );
