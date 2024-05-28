99b32662c23c7fa2eab6e9e50cdf6693e15023dc
Deep Patel <patel_deep1217@outlook.com>
2024-05-28 12:28:05 -0400

    Update hero sectino background to use an image for mobile view

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 function Hero() {
   return (
-    <section className=" md:bg-main  flex min-h-screen items-center justify-center bg-primary-5 bg-cover bg-center text-white ">
+    <section className=" flex  min-h-screen items-center justify-center bg-primary-5 bg-main bg-cover bg-center text-white ">
       <div className=" px-12 text-center md:max-w-5xl">
         <h1 className="text-[3rem] font-bold uppercase lg:text-[4rem]">
           continue exploring
@@ -10,7 +10,7 @@ function Hero() {
           explicabo debitis est autem dicta.
         </p>
 
-        <button className="border-2 bg-white px-6 py-4 text-xl font-light uppercase tracking-widest text-primary-4 shadow transition-colors duration-300 ease-in-out hover:bg-primary-5 hover:text-white">
+        <button className="border-2 bg-white px-6 py-4 text-xl font-light uppercase tracking-widest text-primary-4 shadow transition-colors duration-300 ease-in-out hover:bg-transparent hover:text-white">
           Explore Tours
         </button>
       </div>
